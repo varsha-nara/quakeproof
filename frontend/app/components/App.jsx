053f421ebd444fec7495a8bca30e8441d4010ea7
Varsha Narasiman <vnarasiman@umail.ucsb.edu>
2026-02-09 10:18:32 -0800

    vn - fixed name of backend url

--- frontend/app/components/App.jsx
+++ frontend/app/components/App.jsx
@@ -16,7 +16,7 @@ const DENSITIES = {
   lamp: 40,
   default: 80
 }
-const BACKEND_URL = process.env.BACKEND_URL;
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 function Room({ magnitude }) {
   const rigidBody = useRef()
